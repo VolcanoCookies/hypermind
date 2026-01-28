535385a9381162be1e234eecb077d1b46b617ee8
VolcanoCookies <volcanocookies@gmail.com>
2026-01-28 13:21:25 +0100

    feat: load identity from file if present

--- src/core/identity.js
+++ src/core/identity.js
@@ -1,9 +1,33 @@
 const crypto = require("crypto");
 const { MY_POW_PREFIX } = require("../config/constants");
 const { generateScreenname } = require("../utils/name-generator");
+const { readFileSync } = require("fs");
 
 const generateIdentity = () => {
-    const { publicKey, privateKey } = crypto.generateKeyPairSync("ed25519");
+    let publicKey, privateKey;
+    if (process.env.IDENTITY_PRIVATE_KEY && process.env.IDENTITY_PUBLIC_KEY) {
+        const privateKeyDer = process.env.IDENTITY_PRIVATE_KEY;
+        const publicKeyDer = process.env.IDENTITY_PUBLIC_KEY;
+
+        const privateKeyData = readFileSync(privateKeyDer);
+        const publicKeyData = readFileSync(publicKeyDer);
+
+        privateKey = crypto.createPrivateKey({
+            key: privateKeyData,
+            format: "der",
+            type: "pkcs8",
+        });
+        publicKey = crypto.createPublicKey({
+            key: publicKeyData,
+            format: "der",
+            type: "spki",
+        });
+    } else {
+        const keyPair = crypto.generateKeyPairSync("ed25519");
+        publicKey = keyPair.publicKey;
+        privateKey = keyPair.privateKey;
+    }
+
     const id = publicKey.export({ type: "spki", format: "der" }).toString("hex");
     const screenname = generateScreenname(id);
 
